ab2b178524e100cfeb86c75f54a9338d629c94da
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2024-03-14 13:42:35 +0000

    import dictionary files

--- api/app.py
+++ api/app.py
@@ -4,7 +4,11 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def main():
     return json.dumps({"name": "bob", "number": 3})
+
+def solve(chars):
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
