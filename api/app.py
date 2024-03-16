639cfca032d968317b6e53371d6aed9a698c0e80
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2024-03-16 20:46:11 +0000

    Merge branch 'main' of https://github.com/LegendaryAKx3/flask-anagrams

--- api/app.py
+++ api/app.py
@@ -1,16 +1,33 @@
 from flask import Flask
 import json
+import itertools
 
-with open("dictionary.json") as json_file:
-    dictionary = json.load(json_file)
  
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return json.dumps(dictionary)
+    return solve("gainly")
 
-# def solve(chars):
+def solve(chars):
+    from itertools import permutations,combinations
+    import enchant
+
+    dictionary = enchant.Dict("en_US")
+    chars = list(chars)
+    solutions = []
+
+    # For all words from 2 letters to max letters
+    for i in range(2, len(chars)+1):
+        # Generate all combinations of those letters 
+        for current_set in combinations(chars, i):
+            # Generate all orders of those combinations 
+            for current in permutations(current_set):
+                current_word = ''.join(current)
+                if dictionary.check(current_word)and current_word not in solutions:
+                    solutions.append(current_word)
+    return solutions
+
     
 
 if __name__ == '__main__':
